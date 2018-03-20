02e8cddf8a62dc1ebb8d899f0d9eed8ea81e8a4a
Shoshana Vasserman <shoshievass@Shoshanas-MacBook-Pro.local>
2018-03-20 09:37:06 -0400

    Hopefully fixed integration issue

--- app.R
+++ app.R
@@ -2,6 +2,8 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 library(ggthemes)
+library(rootSolve)
+
 
 ui <- fluidPage(
 
@@ -75,14 +77,21 @@ server <- function(input, output) {
       c <- c_p + c_d
       k <- k_p + k_d
 
-      integrand <- function(x) {
+      integrand_raw <- function(x) {
         S_plus = J + alpha*(c*(t_endpoint - x) + k) - (c_p*(t_endpoint - x) + k_p)
         S = pmax(S_plus, 0.0)
 
         return(S * getConditionalProbFirstArrival(lambda, t, x))
       }
 
-      E_S_t <- 1.0/(1.0 - exp(-lambda * (t_endpoint - t))) * integrate(f = integrand, lower = t, upper = t_endpoint, stop.on.error=FALSE)$value
+      integrand <- function(x){ifelse(x==t_endpoint, 0, integrand_raw(x))}
+
+      tryCatch({
+        E_S_t <- 1.0/(1.0 - exp(-lambda * (t_endpoint - t))) * integrate(f = integrand, lower = t, upper = t_endpoint, stop.on.error=FALSE)$value
+      },
+      error = function(e) {
+        print(paste0("lambda = ", lambda, "; alpha = ", alpha, "; t: ", t, "; endpoint: ", t_endpoint))}
+      )
 
       return(E_S_t)
     }
@@ -99,34 +108,45 @@ server <- function(input, output) {
       return(tstar)
     }
 
-    get_t_star_solve <- function(y, alpha, Tmax, J, c_p, c_d, k_p, k_d){
-      s_equation <- function(t){
-        eq <- get_S_t_of_L(alpha, Tmax, J, c_p, c_d, k_p, k_d, t) - get_s_star(lambda, y, c_p, c_d)
-
-        return(eq)
-      }
-
-      unique_root <- uniroot(s_equation, c(0, Tmax-0.01),extendInt = "yes")$root
-      return(unique_root)
-    }
+    # get_t_star_solve <- function(y, alpha, Tmax, J, c_p, c_d, k_p, k_d){
+    #   s_equation <- function(t){
+    #     eq <- get_Expected_S_t_of_L_point(alpha, Tmax, J, c_p, c_d, k_p, k_d, t) - get_s_star(lambda, y, c_p, c_d)
+    #
+    #     return(eq)
+    #   }
+    #
+    #   unique_root <- uniroot(s_equation, c(0, Tmax-0.01),extendInt = "no")$root
+    #   return(unique_root)
+    # }
 
     get_t_star_star_solve <- function(y, alpha, Tmax, J, c_p, c_d, k_p, k_d){
       print(paste0("E[S_t(Tmax)] = ", get_Expected_S_t_of_L(lambda, alpha, J, c_p, c_d, k_p, k_d, Tmax, (Tmax-0.01))))
       print(paste0("S_star = ", get_s_star(lambda, y, c_p, c_d)))
 
-      if(get_Expected_S_t_of_L(lambda, alpha, J, c_p, c_d, k_p, k_d, Tmax, (Tmax-0.01)) < get_s_star(lambda, y, c_p, c_d)){
-        return(Tmax)
-      }
-      else{
+      # if(get_Expected_S_t_of_L_point(lambda, alpha, J, c_p, c_d, k_p, k_d, Tmax, (Tmax-0.01)) < get_s_star(lambda, y, c_p, c_d)){
+      #   return(Tmax)
+      # }
+      # else{
         s_equation <- function(t){
-          eq <- get_Expected_S_t_of_L(lambda, alpha, J, c_p, c_d, k_p, k_d, Tmax, t) - get_s_star(lambda, y, c_p, c_d)
+          eq <- get_Expected_S_t_of_L_point(lambda, alpha, J, c_p, c_d, k_p, k_d, Tmax, t) - get_s_star(lambda, y, c_p, c_d)
 
           return(eq)
         }
 
-        unique_root <- uniroot(s_equation, c(0, (Tmax-0.01)),extendInt = "yes")$root
-        return(unique_root)
-      }
+        out <- Tmax
+        tryCatch({
+          out <- uniroot(s_equation, c(0, Tmax-0.01),extendInt = "yes")$root
+        },
+        error = function(e) {
+          print("No root found in the specified interval; t** = Tmax")
+          out <- Tmax}
+        )
+
+        return(out)
+
+        # unique_root <- uniroot(s_equation, c(0, (Tmax-0.01)),extendInt = "yes")$root
+        # return(unique_root)
+      # }
     }
 
     get_t_star_star_star_solve <- function(y, alpha, Tmax, J, c_p, c_d, k_p, k_d, lambda){
@@ -141,18 +161,22 @@ server <- function(input, output) {
         return(s_star_ext)
       }
 
-      # if(get_Expected_S_t_of_L(lambda, alpha, J, c_p, c_d, k_p, k_d, Tmax, Tmax-0.01) > get_s_star_star(Tmax-0.01)){
-      #   return(Tmax)
-      # }
-      # else{
         s_equation <- function(t){
-          eq <- get_Expected_S_t_of_L(lambda, alpha, J, c_p, c_d, k_p, k_d, Tmax, t) - get_s_star_star(t)
+          eq <- get_Expected_S_t_of_L_point(lambda, alpha, J, c_p, c_d, k_p, k_d, Tmax, t) - get_s_star_star(t)
 
           return(eq)
         }
 
-        unique_root <- uniroot(s_equation, c(0, Tmax-0.01),extendInt = "yes")$root
-        return(unique_root)
+        out <- Tmax
+        tryCatch({
+          out <- uniroot(s_equation, c(0, Tmax-0.01),extendInt = "yes")$root
+        },
+        error = function(e) {
+          print("No root found in the specified interval; t*** = Tmax")
+          out <- Tmax}
+        )
+
+        return(out)
       # }
     }
 
@@ -318,15 +342,12 @@ server <- function(input, output) {
       }
 
 
-      ggplot(data.frame(x = c(0,Tmax)), aes(x = x)) +
+      settlementplot <- ggplot(data.frame(x = c(0,Tmax)), aes(x = x)) +
         stat_function(fun = S_t, color = "black") +
         annotate(geom="text", label=" -- Expected Settlement Amount: S_t(L)", x=(Tmax*(0.75)), y=(max_y*1.8), color = "black") +
         stat_function(fun = s_star_t, color = "blue", linetype = 2) +
         stat_function(fun = E_S_t, color = "red") +
         annotate(geom="text", label="-- E[S_t(L) | arrival in (t, T)]", x=(Tmax*(0.75)), y=(max_y*1.6), color = "red") +
-        geom_vline(xintercept = t_thresh, linetype = 3) +
-        annotate(geom="text", label=t_thresh_label, x=t_thresh, y=-200, hjust=-0.5) +
-        # geom_vline(xintercept = t_star_star_star_comp, linetype = 3) +
         geom_segment(x = Tmax, xend=Tmax, y=0, yend=S_t(Tmax), color = deadline_color, size=4) +
         stat_function(fun=disagreement, geom="area", aes(fill = "Agree", alpha=0.2)) +
         stat_function(fun=agreement, geom="area", aes(fill = "Disagree", alpha=0.2)) +
@@ -343,6 +364,14 @@ server <- function(input, output) {
         ) +
         scale_y_continuous(breaks = c(s_star), labels = c("s*")) +
         xlim(0, Tmax)
+
+      if(t_thresh < Tmax){
+        settlementplot <- settlementplot +
+          geom_vline(xintercept = t_thresh, linetype = 3) +
+          annotate(geom="text", label=t_thresh_label, x=t_thresh, y=-200, hjust=-0.5)
+      }
+
+      settlementplot
     }
     else{
       print("alpha == alpha_star")
